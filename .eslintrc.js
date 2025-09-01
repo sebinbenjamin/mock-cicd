ef91b83a83a96583decae527245558d8a0567775
Sebin Benjamin <sebinbenjamin@gmail.com>
2025-09-01 20:07:33 +1200

    chore: adds ESLint for code linting
    Adds ESLint configuration with recommended rules to enforce code style and consistency.
    This includes rules for indentation, line breaks, quotes, and semicolons.
    Also configures .eslintignore to exclude node_modules, coverage, dist, and build directories from linting.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "jest": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "commonjs"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
